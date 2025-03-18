130a22d142f96c53a1fae497a104a1e633eb847f
oyindamola Babatola <oyindamola2383@gmail.com>
2025-03-18 16:44:55 +0100

    mestwo

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,12 @@
-// import { WithPWA } from "next-pwa";
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: ["your-image-host.com"],
-//     deviceSizes: [320, 420, 768, 1024, 1200],
-//     imageSizes: [16, 32, 48, 64, 96],
-//   },
-// };
-
-// export default WithPWA({
-//   ...nextConfig, 
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//   },
-// });
-import withPWA from "next-pwa"; // ✅ Correct import
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ["your-image-host.com"],
+    domains: ['your-image-host.com'],
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96],
   },
+  /* config options here */
 };
 
-export default withPWA({
-  ...nextConfig, 
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-
-});
+export default nextConfig;
