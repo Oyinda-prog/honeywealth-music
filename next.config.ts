9ae411e0147917c5604f8359eabbe70857ae919f
oyindamola Babatola <oyindamola2383@gmail.com>
2025-03-18 16:49:48 +0100

    mestwo

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['your-image-host.com'],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
-  },
   /* config options here */
 };
 
